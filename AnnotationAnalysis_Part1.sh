56b7ca2fd25762ab1f3b8768dcacda43c0746597
cjbparlant <cjbparlant@ls28.crchul.ulaval.ca>
2012-01-31 15:20:08 -0500

    Added usage infos

--- AnnotationAnalysis_Part1.sh
+++ AnnotationAnalysis_Part1.sh
@@ -4,7 +4,9 @@
 # $2 basename pour les fichiers produits par le script
 
 if [ ! $# -eq 2 ]; then
-	echo Invalid number of arguments
+	echo 'Usage:'
+	echo 'AnnotationAnalysis_Part1.sh <fileName>'
+	echo 'Note: Script is currently working only with macs output'
 else
 	if [ -a $1 ]; then
 		fileName=$1
@@ -26,6 +28,7 @@ else
 		extractFileName="${basename}_NAs.txt"
 		echo Saving unknown peak identification to: $extractFileName
 		extractNAs.sh $summaryFileName > $extractFileName
+		echo Analyze $extractFileName with DAVID, then run AnnotationAnalysis_Part2.sh
 	else
 		echo Invalid file name $1
 	fi
