7aa5d8ab896c05b488612f4576e5a1c9c758e7c4
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-04-05 15:57:11 -0400

    ...
    Signed-off-by: CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- peakAnno.R
+++ peakAnno.R
@@ -4,7 +4,7 @@ data(TSS.human.NCBI36)
 
 argv <- commandArgs(trailingOnly = T)
 #argv[1] Name of bed file to annotate
-#argv[2] Name of file to save results
+#argv[2] Prefix to save results
 
 print("Starting peak annotation analysis...")
 
@@ -16,11 +16,31 @@ if (length(argv) == 2) {
 	# Annotate and save to file
 	print("Starting batch annotation...")
 	annotatedPeak = annotatePeakInBatch(SequencesRD, AnnotationData=TSS.human.NCBI36)
+	print("Trying to produce figure...")
+	y = annotatedPeak$distancetoFeature[!is.na(annotatedPeak$distancetoFeature) 
+	& annotatedPeak$fromOverlappingOrNearest == "NearestStart"]
+	tiff_name = paste (argv[2], "_hist.tiff", sep="")
+	tiff(tiff_name)
+#	hist(y, xlab="Distance To Nearest TSS", main="", breaks=1000, xlim=c(min(y)-100, max(y)+100)) 
+	hist(y, xlab="Distance To Nearest TSS", main="", breaks=1000, xlim=c(-200000, 200000))
+	dev.off()
+	temp = as.data.frame(annotatedPeak) 
+	y = annotatedPeak$distancetoFeature[!is.na(annotatedPeak$distancetoFeature) 
+	& annotatedPeak$fromOverlappingOrNearest == "NearestStart" & 
+	abs(annotatedPeak$distancetoFeature) <10000]
+	tiff_name = paste (argv[2], "_pie.tiff", sep="")
+	tiff(tiff_name)
+	pie(table(temp[as.character(temp$fromOverlappingOrNearest) == "Overlapping" | 
+	(as.character(temp$fromOverlappingOrNearest) == "NearestStart" & !temp$peak
+	%in% temp[as.character(temp$fromOverlappingOrNearest) == "Overlapping", ]
+	$peak) ,]$insideFeature))
+	dev.off()
 	print("Adding gene IDs...")
 	IDs_df <- as.data.frame(addGeneIDs(annotatedPeak, "org.Hs.eg.db", IDs2Add=c("symbol", "genename", "refseq")))
 	print("Saving...")
+	save_name = paste(argv[2], "_ChipPeakAnno.out")
 	toSave <- IDs_df[,c('space', 'start', 'end', 'width', 'strand', 'feature', 'start_position', 'end_position', 'insideFeature', 'distancetoFeature', 'shortestDistance', 'fromOverlappingOrNearest', 'symbol', 'refseq', 'genename')]
-	write.table(toSave, file=argv[2], row.names=FALSE, quote=FALSE, sep="\t")
+	write.table(toSave, file=save_name, row.names=FALSE, quote=FALSE, sep="\t")
 } else {
 	print("Incorrect number of arguments")
 }
